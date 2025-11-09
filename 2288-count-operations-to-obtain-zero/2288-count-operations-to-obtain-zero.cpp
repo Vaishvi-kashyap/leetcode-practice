1022cefd066e113594169750ef72422798437d63
Vaishvi Kashyap <83880143+Vaishvi-kashyap@users.noreply.github.com>
2025-11-10 01:02:09 +0530

    Time: 0 ms (100%), Space: 8 MB (40.22%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
+    int ans =0;
+    void help(int num1, int num2){
+        if(num1==0 || num2==0) return;
+        if(num1<num2) return help(num2,num1);
+        ans++;
+        help(num1-num2,num2);
+
+    }
     int countOperations(int num1, int num2) {
-        int count = 0;
-        while (num1 != 0 && num2 != 0) {
-            if (num1 >= num2) {
-                count += num1 / num2; // how many times num2 fits in num1
-                num1 = num1 % num2;
-            } else {
-                count += num2 / num1;
-                num2 = num2 % num1;
-            }
-        }
-        return count;
+        help(num1,num2);
+        return ans;
     }
 };
