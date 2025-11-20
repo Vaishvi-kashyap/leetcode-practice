eeebf9ba3f36f81869f66bd9954e5edd8bd7e247
Vaishvi Kashyap <83880143+Vaishvi-kashyap@users.noreply.github.com>
2025-11-21 03:10:58 +0530

    Time: 7 ms (52.74%), Space: 15 MB (86.28%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -5,7 +5,7 @@ public:
     int val;
     Node* next;
     Node* random;
-    
+
     Node(int _val) {
         val = _val;
         next = NULL;
@@ -17,29 +17,38 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-       if (head == NULL) {
+        if (head == NULL)
             return NULL;
-        }
-        unordered_map<Node*, Node*> m;
-        Node* newHead = new Node(head->val);
-        Node* oldTemp = head->next;
-        Node* newTemp = newHead;
-        m[head] = newHead;
 
-        while (oldTemp != NULL) {
-            Node* newNode = new Node(oldTemp->val);
-            m[oldTemp] = newNode;
-            newTemp->next = newNode;
-            oldTemp = oldTemp->next;
-            newTemp = newTemp->next;
+        Node* temp = head;
+
+        // 1. Insert cloned nodes in between original nodes
+        while (temp != NULL) {
+            Node* copy = new Node(temp->val);
+            copy->next = temp->next;
+            temp->next = copy;
+            temp = copy->next;
         }
-        oldTemp = head;
-        newTemp = newHead;
-        while (oldTemp != NULL) {
-            newTemp->random = m[oldTemp->random];
-            oldTemp = oldTemp->next;
-            newTemp = newTemp->next;
+
+        // 2. Set random pointers in cloned nodes
+        temp = head;
+        while (temp != NULL) {
+            if (temp->random != NULL)
+                temp->next->random = temp->random->next;
+            temp = temp->next->next;
         }
+
+        // 3. Detach the two lists
+        temp = head;
+        Node* newHead = head->next;
+        while (temp != NULL) {
+            Node* copy = temp->next;
+            temp->next = copy->next;
+            if (copy->next != NULL)
+                copy->next = copy->next->next;
+            temp = temp->next;
+        }
+
         return newHead;
     }
 };
