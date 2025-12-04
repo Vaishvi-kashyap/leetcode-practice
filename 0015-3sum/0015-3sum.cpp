cf36c236f459dab0a1891754d22504ee825ffdf5
Vaishvi Kashyap <83880143+Vaishvi-kashyap@users.noreply.github.com>
2025-12-04 10:38:28 +0530

    Time: 48 ms (58.19%), Space: 29.1 MB (47.1%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,7 +5,7 @@ public:
         vector<vector<int>> res;
         sort(nums.begin(), nums.end());
         for (int i = 0; i < n; i++) {
-            if (i < 0 && nums[i] == nums[i - 1])
+            if (i > 0 && nums[i] == nums[i - 1])
                 continue;
             int j = i + 1, k = n - 1;
             while (j < k) {
@@ -20,9 +20,9 @@ public:
                         j++;
                     while (j < k && nums[k] == nums[k - 1])
                         k--;
+                    j++;
+                    k--;
                 }
-                j++;
-                k--;
             }
         }
         return res;
