9b9893247ca8aadeccd24565c329538543053b8a
Vaishvi Kashyap <83880143+Vaishvi-kashyap@users.noreply.github.com>
2025-12-14 09:54:35 +0530

    Time: 0 ms (100%), Space: 13.3 MB (74.73%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size(), n = matrix[0].size();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == target) {
-                    return true;
-                }
-            }
+    bool helperBinarySearch(vector<vector<int>>& matrix, int row, int tar) {
+        int n = matrix[0].size(), st = 0, end = n - 1;
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            if (tar == matrix[row][mid])
+                return true;
+            else if (tar > matrix[row][mid])
+                st = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return false;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int tar) {
+        int m = matrix.size(), n = matrix[0].size(), st = 0, end = m - 1;
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            if (tar >= matrix[mid][0] && tar <= matrix[mid][n - 1])
+                return helperBinarySearch(matrix, mid, tar);
+            else if (tar > matrix[mid][n - 1])
+                st = mid + 1;
+            else
+                end = mid - 1;
         }
         return false;
     }
