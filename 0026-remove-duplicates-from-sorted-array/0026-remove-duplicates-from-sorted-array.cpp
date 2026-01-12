c5e0e41d18cd836261e13585d480c4ce393885a5
Vaishvi Kashyap <83880143+Vaishvi-kashyap@users.noreply.github.com>
2026-01-12 07:49:07 +0530

    Time: 0 ms (100%), Space: 22.7 MB (49.52%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        // We use two pointers: one to track the last unique element and another
+        // to scan the array; when a new value is found, we overwrite the next
+        // position.
         int n = nums.size();
         int i = 0;
         for (int j = 1; j < n; j++) {
