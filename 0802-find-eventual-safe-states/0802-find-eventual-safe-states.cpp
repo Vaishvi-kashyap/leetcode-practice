f08446a2d531cf1f6327be66737f68d5bb8f860a
Vaishvi Kashyap <83880143+Vaishvi-kashyap@users.noreply.github.com>
2026-02-01 02:21:15 +0530

    Time: 59 ms (17.25%), Space: 66.3 MB (22.6%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,43 +1,42 @@
 class Solution {
 public:
-    bool dfs(int src, vector<vector<int>>& graph, vector<bool>& visited,
-             vector<bool>& pathVisited, vector<int>& res,
-             vector<int>& safeNode) {
-        visited[src] = true;
-        pathVisited[src] = true;
-        safeNode[src] = 0;
+    vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
+        int V = adj.size();
 
-        for (int neighbor : graph[src]) {
-            if (!visited[neighbor]) {
-                if (dfs(neighbor, graph, visited, pathVisited, res, safeNode)) {
-                    safeNode[src] = 0;
-                    return true;
-                }
-            } else if (pathVisited[neighbor]) {
-                safeNode[src] = 0;
-                return true;
+        vector<vector<int>> revAdj(V);
+        vector<int> outDegree(V, 0);
+
+        // build reverse graph and count outdegree
+        for (int i = 0; i < V; i++) {
+            for (int next : adj[i]) {
+                revAdj[next].push_back(i);
+                outDegree[i]++; // count outgoing edges
             }
         }
-        safeNode[src] = 1;
-        pathVisited[src] = false;
-        return false;
-    }
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int V = graph.size();
-        vector<bool> visited(V, false);
-        vector<bool> pathVisited(V, false);
-        vector<int> safeNode(V, 0);
-        vector<int> res;
 
+        queue<int> q;
+        vector<int> ans;
+
+        // push terminal nodes (outDegree == 0)
         for (int i = 0; i < V; i++) {
-            if (!visited[i]) {
-                dfs(i, graph, visited, pathVisited, res, safeNode);
+            if (outDegree[i] == 0)
+                q.push(i);
+        }
+
+        // Kahn's BFS
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+
+            for (int prev : revAdj[node]) {
+                outDegree[prev]--;
+                if (outDegree[prev] == 0)
+                    q.push(prev);
             }
         }
-        for (int i = 0; i < V; i++) {
-            if (safeNode[i] == 1)
-                res.push_back(i);
-        }
-        return res;
+
+        sort(ans.begin(), ans.end()); // LeetCode needs sorted
+        return ans;
     }
 };
