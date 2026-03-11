f9b14c9149f625cdd0eba965d1425ae49a668c7c
Vaishvi Kashyap <83880143+Vaishvi-kashyap@users.noreply.github.com>
2026-03-11 08:03:47 +0530

    Time: 0 ms (100%), Space: 20.9 MB (5.25%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,6 +4,7 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
         bool firstRow = false, firstCol = false;
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
