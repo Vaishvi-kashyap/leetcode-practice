6bd54ed43b06bb30bef63c40ba77b8b46ab18516
Vaishvi Kashyap <83880143+Vaishvi-kashyap@users.noreply.github.com>
2026-03-18 13:14:46 +0530

    Time: 7 ms (42.53%), Space: 16 MB (5.23%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> m;
+        unordered_map<int, int> m; // no, index
         int n = nums.size();
         vector<int> ans;
         for (int i = 0; i < n; i++) {
