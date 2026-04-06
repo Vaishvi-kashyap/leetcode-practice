b5e79a5457f5b5da75e56f29f892718c910b5e87
Vaishvi Kashyap <83880143+Vaishvi-kashyap@users.noreply.github.com>
2026-04-06 12:54:54 +0530

    Time: 0 ms (100%), Space: 15 MB (40.49%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -16,14 +16,17 @@ public:
         vector<int> ans;
         if (root == NULL)
             return ans;
+
         queue<TreeNode*> q;
         q.push(root);
 
         while (!q.empty()) {
             int n = q.size();
+
             for (int i = 0; i < n; i++) {
                 TreeNode* curr = q.front();
                 q.pop();
+
                 if (i == n - 1)
                     ans.push_back(curr->val);
                 if (curr->left)
