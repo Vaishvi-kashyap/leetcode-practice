2735fa80d48679d52c6489109f60d17619a56fe7
Vaishvi Kashyap <83880143+Vaishvi-kashyap@users.noreply.github.com>
2026-04-28 10:04:33 +0530

    Time: 12 ms (28.95%), Space: 29.3 MB (88.78%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& grid, vector<vector<bool>>& vis, int row,
-             int col) {
-        int m = grid.size();
-        int n = grid[0].size();
-
-        if (row < 0 || row >= m || col < 0 || col >= n || vis[row][col] ||
-            grid[row][col] != 1)
-            return;
-
-        vis[row][col] = true;
-
-        dfs(grid, vis, row + 1, col);
-        dfs(grid, vis, row - 1, col);
-        dfs(grid, vis, row, col + 1);
-        dfs(grid, vis, row, col - 1);
-    }
     int numEnclaves(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        int cnt = 0;
-        vector<vector<bool>> vis(m, (vector<bool>(n, false)));
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
+        queue<pair<int, int>> q;
 
-        // left & right boundaries
         for (int i = 0; i < m; i++) {
-            if (!vis[i][0] && grid[i][0] == 1)
-                dfs(grid, vis, i, 0);
-            if (!vis[i][n - 1] && grid[i][n - 1] == 1)
-                dfs(grid, vis, i, n - 1);
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == m - 1 || j == 0 || j == n - 1) {
+                    if (!vis[i][j] && grid[i][j] == 1) {
+                        vis[i][j] = true;
+                        q.push({i, j});
+                    }
+                }
+            }
         }
 
-        // top & bottom boundaries
-        for (int j = 0; j < n; j++) {
-            if (!vis[0][j] && grid[0][j] == 1)
-                dfs(grid, vis, 0, j);
-            if (!vis[m - 1][j] && grid[m - 1][j] == 1)
-                dfs(grid, vis, m - 1, j);
+        int delrow[] = {-1, 0, +1, 0};
+        int delcol[] = {0, -1, 0, +1};
+
+        while (!q.empty()) {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            for (int i = 0; i < 4; i++) {
+                int nrow = delrow[i] + row;
+                int ncol = delcol[i] + col;
+                if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n &&
+                    !vis[nrow][ncol] && grid[nrow][ncol] == 1) {
+                    vis[nrow][ncol] = 1;
+                    q.push({nrow, ncol});
+                }
+            }
         }
 
-        // convert surrounded regions
+        int cnt = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (!vis[i][j] && grid[i][j] == 1)
