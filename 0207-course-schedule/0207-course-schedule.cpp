fde652d5a1e0ce49ed59e33e1b82b21d4b8e37b4
Vaishvi Kashyap <83880143+Vaishvi-kashyap@users.noreply.github.com>
2026-05-08 09:27:59 +0530

    Time: 5 ms (54.15%), Space: 19.1 MB (80.7%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,39 +2,40 @@ class Solution {
 public:
     bool canFinish(int V, vector<vector<int>>& edges) {
         vector<vector<int>> adj(V);
-        for (auto e : edges) {
-            int u = e[0];
-            int v = e[1];
+        for (int i = 0; i < edges.size(); i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+
             adj[u].push_back(v);
         }
 
-        vector<int> inDegree(V, 0);
-
-        for (int i = 0; i < V; i++) {
-            for (int it : adj[i]) {
-                inDegree[it]++;
-            }
-        }
-
+        vector<int> indegree(V, 0);
         queue<int> q;
-        int count = 0;
+        int cnt = 0;
         for (int i = 0; i < V; i++) {
-            if (inDegree[i] == 0) {
-                q.push(i);
-            }
-        }
-        while (!q.empty()) {
-            int src = q.front();
-            q.pop();
-            count++;
-            for (int neighbor : adj[src]) {
-                inDegree[neighbor]--;
-                if (inDegree[neighbor] == 0)
-                    q.push(neighbor);
+            for (auto it : adj[i]) {
+                indegree[it]++;
             }
         }
 
-        if (count == V)
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            cnt++;
+
+            for (auto it : adj[curr]) {
+                indegree[it]--;
+                if (indegree[it] == 0)
+                    q.push(it);
+            }
+        }
+
+        if (cnt == V)
             return true;
         return false;
     }
