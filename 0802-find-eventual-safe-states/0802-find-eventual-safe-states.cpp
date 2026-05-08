a584f7b43e847f28c140c11c867599ebc6a93a1f
Vaishvi Kashyap <83880143+Vaishvi-kashyap@users.noreply.github.com>
2026-05-08 15:19:03 +0530

    Time: 53 ms (31.98%), Space: 66.4 MB (20.81%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -6,36 +6,33 @@ public:
         vector<vector<int>> revAdj(V);
         vector<int> outDegree(V, 0);
 
-        // build reverse graph and count outdegree
         for (int i = 0; i < V; i++) {
-            for (int next : adj[i]) {
-                revAdj[next].push_back(i);
-                outDegree[i]++; // count outgoing edges
+            for (auto it : adj[i]) {
+                revAdj[it].push_back(i);
+                outDegree[i]++;
             }
         }
 
         queue<int> q;
         vector<int> ans;
 
-        // push terminal nodes (outDegree == 0)
         for (int i = 0; i < V; i++) {
             if (outDegree[i] == 0)
                 q.push(i);
         }
 
-        // Kahn's BFS
         while (!q.empty()) {
-            int node = q.front();
+            int curr = q.front();
             q.pop();
-            ans.push_back(node);
 
-            for (int prev : revAdj[node]) {
-                outDegree[prev]--;
-                if (outDegree[prev] == 0)
-                    q.push(prev);
+            ans.push_back(curr);
+
+            for (auto it : revAdj[curr]) {
+                outDegree[it]--;
+                if (outDegree[it] == 0)
+                    q.push(it);
             }
         }
-
         sort(ans.begin(), ans.end());
         return ans;
     }
