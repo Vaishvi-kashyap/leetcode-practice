8885bbcdf564a85bc17aa07f46267c7a44c9045e
Vaishvi Kashyap <83880143+Vaishvi-kashyap@users.noreply.github.com>
2026-05-13 10:31:36 +0530

    Time: 0 ms (100%), Space: 18.7 MB (5.12%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -12,17 +12,28 @@
  */
 class Solution {
 public:
-    bool helper(TreeNode* left, TreeNode* right) {
-        if (left == NULL || right == NULL)
-            return left == right;
-        if (left->val != right->val)
-            return false;
-        return helper(left->left, right->right) &&
-               helper(left->right, right->left);
-    }
     bool isSymmetric(TreeNode* root) {
         if (root == NULL)
             return true;
-        return helper(root->left, root->right);
+        queue<TreeNode*> q;
+        q.push(root->left);
+        q.push(root->right);
+        while (!q.empty()) {
+            TreeNode* left = q.front();
+            q.pop();
+            TreeNode* right = q.front();
+            q.pop();
+
+            if (left == NULL && right == NULL)
+                continue;
+            if (left == NULL || right == NULL || left->val != right->val)
+                return false;
+            q.push(left->left);
+            q.push(right->right);
+
+            q.push(left->right);
+            q.push(right->left);
+        }
+        return true;
     }
 };
