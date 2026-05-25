9ae92a71c74d1be3f19a14d80192dfe5b250a938
Vaishvi Kashyap <83880143+Vaishvi-kashyap@users.noreply.github.com>
2026-05-26 01:02:40 +0530

    Time: 6 ms (46.52%), Space: 23.9 MB (59.16%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,9 +3,7 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int n = intervals.size();
         vector<vector<int>> ans;
-
         sort(intervals.begin(), intervals.end());
-
         for (int i = 0; i < n; i++) {
             if (ans.empty() || intervals[i][0] > ans.back()[1])
                 ans.push_back(intervals[i]);
