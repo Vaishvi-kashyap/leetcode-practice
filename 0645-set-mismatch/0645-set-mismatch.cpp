95fcae28d8a694844983b716ae01ccfb6aac0bb8
Vaishvi Kashyap <83880143+Vaishvi-kashyap@users.noreply.github.com>
2026-05-27 03:41:30 +0530

    Time: 0 ms (100%), Space: 26.3 MB (40.07%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -3,23 +3,16 @@ public:
     vector<int> findErrorNums(vector<int>& nums) {
         int n = nums.size();
         vector<int> freq(n + 1, 0);
+        for (int val : nums)
+            freq[val]++;
 
-        for (int num : nums) {
-            freq[num]++;
-        }
-
-        int duplicate = -1;
-        int missing = -1;
-
+        int duplicate = -1, missing = -1;
         for (int i = 1; i <= n; i++) {
-            if (freq[i] == 2) {
+            if (freq[i] == 2)
                 duplicate = i;
-            }
-            if (freq[i] == 0) {
+            if (freq[i] == 0)
                 missing = i;
-            }
         }
-
         return {duplicate, missing};
     }
 };
