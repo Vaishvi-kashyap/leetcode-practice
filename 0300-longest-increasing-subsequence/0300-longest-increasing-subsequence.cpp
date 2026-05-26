65f5f03cefa20386afbe2a142ca8806abc653ff6
Vaishvi Kashyap <83880143+Vaishvi-kashyap@users.noreply.github.com>
2026-05-27 03:17:27 +0530

    Time: 59 ms (60.53%), Space: 14.3 MB (57.02%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -4,10 +4,12 @@ public:
         int n = nums.size();
         vector<int> dp(n, 1);
         int ans = 1;
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i])
+                if (nums[i] > nums[j]) {
                     dp[i] = max(dp[i], dp[j] + 1);
+                }
             }
             ans = max(ans, dp[i]);
         }
