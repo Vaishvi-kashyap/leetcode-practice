2661926696c52b26e1448ed5f4a86185a0edb0c6
Vaishvi Kashyap <83880143+Vaishvi-kashyap@users.noreply.github.com>
2026-06-02 21:57:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -4,9 +4,9 @@ public:
         unordered_set<int> st(nums.begin(), nums.end());
         int longest = 0;
 
-        for (int num : st) {
-            if (st.find(num - 1) == st.end()) {
-                int start = num;
+        for (int val : nums) {
+            if (st.find(val - 1) == st.end()) {
+                int start = val;
                 int count = 1;
                 while (st.find(start + 1) != st.end()) {
                     start++;
