c1d20e799c573e8cdc7f147aae1e0ae2b78ffc3c
Vaishvi Kashyap <83880143+Vaishvi-kashyap@users.noreply.github.com>
2026-09-15 09:00:10 +0530

    Time: 3 ms (58.53%), Space: 117.9 MB (81.64%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -13,6 +13,7 @@ public:
     bool isPalindrome(ListNode* head) {
         ListNode* slow = head;
         ListNode* fast = head;
+
         while (fast && fast->next) {
             slow = slow->next;
             fast = fast->next->next;
